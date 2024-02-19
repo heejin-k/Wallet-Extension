93a6cf6da11b3aedf23323cd1a873808bdd35aa4
Elena <elena@hessegg.com>
2024-02-19 16:35:56 +0900

    remove: header 폴더 위치 변경
    header 폴더구조 변경

---
+++ src/components/module/header/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { HeaderWrap } from "./headerStyle";
+
+const Header = () => {
+  return(
+  <>
+    <HeaderWrap>header</HeaderWrap>
+  </>
+  )
+};
+
+export default Header;
---
+++ src/components/module/header/headerStyle.jsx
@@ -0,0 +1,8 @@
+import styled from 'styled-components'
+import { palette } from "style/theme";
+
+
+export const HeaderWrap = styled.h1`
+font-size: 20px;
+color: ${palette.pointColor};
+`
