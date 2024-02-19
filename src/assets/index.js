1573775a362e5575c9ac9852b50548f9a66ecb6e
Elena <elena@hessegg.com>
2024-02-19 17:18:32 +0900

    rename: change icon name
    icn__check--fill--16.svg로 되어있던 아이콘 이름을 icn_check_fill_16.svg 형식으로 변경

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,2 +1,2 @@
-export { default as ErrorIcon } from './icn__exclamation--fill--16.svg';
-export { default as CheckIcon } from './icn__check--fill--16.svg';
+export { default as ErrorIcon } from './icn_exclamation_fill_16.svg';
+export { default as CheckIcon } from './icn_check_fill_16.svg';
