6c6fdb1f7864ebb1b55daf099f5874ae737de27a
Elena <elena@hessegg.com>
2024-02-20 14:38:14 +0900

    fix: browserRouter change to MemoryRouter
    익스텐션에서 라우팅을 위해 browserRouter를 MemoryRouter로 변경

--- src/index.js
+++ src/index.js
@@ -3,14 +3,14 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import 'style';
 import GlobalStyle from 'style/Globalstyles';
-import { BrowserRouter } from 'react-router-dom';
+import { MemoryRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <MemoryRouter>
       <GlobalStyle />
       <App />
-    </BrowserRouter>
+    </MemoryRouter>
   </React.StrictMode>
 );
